78236e11b99a68891d7473826c1dbc89ae12afcc
JamesYuuu <yujunda20@mails.ucas.ac.cn>
2022-08-03 16:01:33 +0800

    Fix Small Issue

--- sanic/routes.py
+++ sanic/routes.py
@@ -3,5 +3,5 @@ from views.view_select import get_image
 from views.view_download import download
 
 def add_route(app:Sanic):
-    app.add_route(get_image,'/select',methods='POST')
-    app.add_route(download,'/download',methods='POST')
+    app.add_route(get_image,'/select',methods=['POST'])
+    app.add_route(download,'/download',methods=['POST'])
